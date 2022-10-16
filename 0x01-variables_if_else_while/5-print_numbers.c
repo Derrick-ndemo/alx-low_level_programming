66f53fb606d6ffaa3e34ef9b266f926773d86166
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-17 01:28:27 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Printing numbers form 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
