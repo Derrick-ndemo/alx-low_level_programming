b14b976caf7640221f70aee2fc38351b7a494d1c
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-17 02:18:46 +0300

    All different possibel number combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0, mum1;
+
+	while (num <= 9)
+	{
+		num1 = 0;
+		while (num1 <- 9)
+		{
+			if (num != num1 && num < num1)
+			{
+				putchar(num + 48);
+				putchar(num1 + 48);
+
+				if (num + num1 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++num1;
+		}
+		++num;
+	}
+	putchar('\n');
+
+	return (0);
+}
