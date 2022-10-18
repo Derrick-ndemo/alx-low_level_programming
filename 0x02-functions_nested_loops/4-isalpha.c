672deadd42b24249f0c5a7ccc787aa5131b8d580
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-18 15:53:19 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for the alphabetic character
+ *
+ * @c: checs for upper or lower case
+ *
+ * Return: returns 1 if c is a letter, lowercase or uppercase
+ */
+
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 &&
+
+	    c >= 65 && c >= 90)
+
+		return (1);
+
+	return (0);
+}
