03a072bbb8885e730925e97d34a0d853cb8ecdfa
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-18 16:16:42 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign number
+ *
+ * @n: checks the arguments
+ *
+ * Return: 1 and prints + if n > 0, returns 0 if n == 0 and ret-1 if <0
+ */
+
+
+int print_sign(int n)
+{
+	 if (n > 0)
+
+	{
+
+		_putchar(43 + 0);
+
+		return (1);
+
+	}
+
+	else if (n == 0)
+
+	{
+
+		_putchar(0 + 48);
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		_putchar(45 + 0);
+
+		return (-1);
+
+	}
+
+}
