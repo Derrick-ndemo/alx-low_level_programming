3acf5b509f6e69dd4c72c4ab6d3cec902e50fcb2
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-18 14:05:51 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,37 @@
-#include "main.h"
+#include"main.h"
+
+
 
 /**
-*main - Entry point
-* 
-*Descript - printing _putchar using prototype
-*
-*Return: Always 0 (Success)
+
+ * main - Entry point
+
+ *
+
+ * Description: prints _putchar using putchar prototype
+
+ *
+
+ * Return: Always 0 (Success)
+
 */
 
+
+
 int main(void)
+
 {
+
 	char str[] = "_putchar";
+
 	int ch;
 
-	for (ch = 0; ch < 0; ++ch)
-	{
+
+
+	for (ch = 0; ch < 8; ++ch)
+
 		_putchar(str[ch]);
-	}
+
 	_putchar('\n');
 
 
