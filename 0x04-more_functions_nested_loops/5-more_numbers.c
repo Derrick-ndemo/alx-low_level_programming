c25e70818ac609c8b3e78b6ca9d478cba56858be
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-21 15:26:52 -0700

    printing 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,23 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j = 0;
 
-	for (i = 0; i <= 14; ++i)
+	int num, row, count;
+
+	for (row = 1; row <= 10; ++row)
 	{
-		do {
-			_putchar(i + 48);
-			++j;
-		} while (j <= 10);
+		for (count = 0; count <= 14; ++count)
+		{
+			num = count;
+
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
