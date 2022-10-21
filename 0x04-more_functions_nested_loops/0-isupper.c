8d7c687c25ccd7b3b487e926d7119e11c34f4afd
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-21 11:28:48 -0700

    checking for uppercase letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,23 @@
-
 /**
- * _isupper - checks if c is uppercase
+ * _isupper - check if c is upper
  *
- * @c: parameter representing ASCII values of upr case letters
+ * @c: input for alphabet
  *
- * Return: 1 if c is uppercase otherwise 0
- */
+ * Return: 1 if its uppercase, 0 if not
+*/
+
+
 
 int _isupper(int c)
+
 {
-	if (c >= 65 && <= 90)
+
+	if (c >= 65 && c <= 90)
+
 		return (1);
+
 	else
+
 		return (0);
+
 }
