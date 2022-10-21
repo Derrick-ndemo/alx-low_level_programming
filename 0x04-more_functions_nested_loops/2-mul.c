1d503175f9d8e2b1bdb72481ecb9bc3174a14b33
Derrick-ndemo <derrickndemo@gmail.com>
2022-10-21 12:05:32 -0700

    multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * mul - Multiplies two intagers
+ *
+ * @a: represents the first intager digit
+ * @b: represents the second intager
+ *
+ * Return: Always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
