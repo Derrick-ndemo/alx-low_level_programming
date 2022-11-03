01a65df3cd7cd1fff285f0b1d33f8b92a5c12801
Derrick-ndemo <derrickndemo@gmail.com>
2022-11-03 18:18:50 +0300

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - function that returns power
+ *
+ * @x: reps the number
+ * @y: what number to power to
+ *
+ * Return: Power
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (_pow_recursion(x ** y);
+} 
