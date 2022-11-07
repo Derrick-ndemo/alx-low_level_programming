7d9d0a3ce826fb3c1680ced23525f575b2ded8f7
Derrick-ndemo <derrickndemo@gmail.com>
2022-11-08 01:08:11 +0300

    printing the number of items

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - programme that prints number of arguments passed
+ *
+ * @argc: number of commands
+ * @argv: array of values
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i = 0, n;
+
+	while (i < argc)
+	{
+		n = i;
+		i++;
+	}
+	printf("%d\n", m);
+	return (0);
+}
