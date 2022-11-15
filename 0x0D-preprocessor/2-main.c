57676221f8a836136e17b728136f6d1e4aadf7f4
Derrick-ndemo <derrickndemo@gmail.com>
2022-11-15 17:46:29 +0300

    Compiled location file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints the name of file it
+ * 	 was compiled from, followed
+ * 	 by a new line
+ * Return: Always 0 if success
+*/
+
+int main(void)
+{
+	_putchar(__FILE__);
+	_putchar('\n');
+
+	return (0);
+}
