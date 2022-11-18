558812047a340ba3d0d8444b0e39a5d4a4755222
Derrick-ndemo <derrickndemo@gmail.com>
2022-11-19 00:34:08 +0300

    Minor code fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,17 +15,18 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	int sum = 0;
+	unsigned int sum = 0;
 
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
-		int x = va_arg(args, int);
+		unsigned int x = va_arg(args, int);
 
 		if (n == 0)
 			sum = 0;
 		else if (n > 0)
 			sum += x;
 	}
+	va_end(args);
 
 	return (sum);
 }
