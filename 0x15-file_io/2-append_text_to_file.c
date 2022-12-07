97ab90baf1beb32e25c3c9a15df583fbe4b6d905
Derrick-ndemo <derrickndemo@gmail.com>
2022-12-07 22:23:08 +0300

    minor fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	file = open(filename, O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 	w = write(fd, text_content, len);
 
 	if (file == -1 || w == -1)
