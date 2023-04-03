422c46b3bd7e9fa577ac1dc3f084b68deadca72e
Derrick-ndemo <derrickndemo@gmail.com>
2023-04-03 15:00:31 +0300

    betty coding style; git push

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,39 +9,33 @@
 
 int main(void)
 {
-  int a = 0, b, c;
-  
-  while (a <= 9)
-  {
-    b = 0;
-    while (b <= 9)
-    {
-      c = 0;
-      while (c <= 9)
-      {
-        if (a != b && a != c && b > a && c > a && c > b)
-        {
-          putchar(48 + a);
-          putchar(48 + b);
-          putchar(48 + c);
+	int a = 0, b, c;
 
-          if (a + b != 17)
-          {
-            putchar(',');
-            putchar(' ');
-          }
-        }
-        ++c;
-      }
-      ++b;
-    }
-    
-    ++a;
-  }
-  
-  putchar('\n');
-
-  return (0);
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			c = 0;
+			while (c <= 9)
+			{
+				if (a != b && a != c && b > a && c > a && c > b)
+				{
+					putchar(48 + a);
+					putchar(48 + b);
+					putchar(48 + c);
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				++c;
+			}
+			++b;
+		}
+		++a;
+	}
+	putchar('\n');
+	return (0);
 }
-
-
